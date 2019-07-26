d0677906b9ac55186e67a7155b1c4396c5d00e6f
massimo cacciapaglia <cacciapaglia@baxenergy.com>
2019-07-26 11:02:03 +0200

    Fixed index placeholders

--- resources/index.php
+++ resources/index.php
@@ -34,30 +34,30 @@ function __registerBlockFunctionName____register_block() {
 	}
 
 	wp_register_script(
-		'plugin-prefix-esnext',
+		'plugin-prefix-plugin-name-internal',
 		plugins_url( 'build/index.js', __FILE__ ),
 		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ),
 		filemtime( plugin_dir_path( __FILE__ ) . 'build/index.js' )
 	);
 
 	wp_register_style(
-		'plugin-prefix-esnext-editor',
+		'plugin-prefix-plugin-name-internal-editor',
 		plugins_url( 'editor.css', __FILE__ ),
 		array( 'wp-edit-blocks' ),
 		filemtime( plugin_dir_path( __FILE__ ) . 'editor.css' )
 	);
 
 	wp_register_style(
-		'plugin-prefix-esnext',
+		'plugin-prefix-plugin-name-internal',
 		plugins_url( 'style.css', __FILE__ ),
 		array( ),
 		filemtime( plugin_dir_path( __FILE__ ) . 'style.css' )
 	);
 
 	register_block_type( 'plugin-prefix/plugin-name-internal', array(
-		'style' => 'plugin-prefix-esnext',
-		'editor_style' => 'plugin-prefix-esnext-editor',
-		'editor_script' => 'plugin-prefix-esnext',
+		'style' => 'plugin-prefix-plugin-name-internal',
+		'editor_style' => 'plugin-prefix-plugin-name-internal-editor',
+		'editor_script' => 'plugin-prefix-plugin-name-internal',
 	) );
 
 }
